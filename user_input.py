6e25e5fe419753669ccce29691042fa954dc792e
Caleb M Maina <160316535+blazinbanana@users.noreply.github.com>
2024-03-04 08:02:53 -0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input('Enter your name: ')
+age=input('Enter your age: ')
+location=input('Where are you currently staying: ')
+print('Hello', name, 'You are ',age, 'years old and live in ',location,'. Nice to meet you.')
